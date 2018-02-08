20a5283c838f1ed59d0472d36a7f79a159bf3cce
hanford <jackhanford@gmail.com>
2018-02-08 00:36:40 -0800

    whitespace

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const defaultOpts = {
 
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
-
-    webpack(config, options) {
+    webpack (config, options) {
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
