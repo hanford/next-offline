7bb0986a76d13750daec577ce5b5f5ddc15ef1b5
Jack Hanford <jackhanford@gmail.com>
2018-11-10 17:08:28 -0800

    add tests, revert async webpack function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const exportSw = require('./export');
 module.exports = (nextConfig = {}) => ({
   ...nextConfig,
   exportPathMap: exportSw(nextConfig),
-  async webpack(config, options) {
+  webpack(config, options) {
     if (!options.defaultLoaders) {
       throw new Error(
         'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade',
