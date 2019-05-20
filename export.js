5d650b95299e3ec830400e09d2fab59997ca66e6
Airam <airamrguez@users.noreply.github.com>
2019-05-20 18:29:56 +0200

    fix exportPathMap not called in dev mode
    When running in dev mode `exportPathMap` should also be executed as this is the default behaviour in Next.js.

--- export.js
+++ export.js
@@ -7,12 +7,10 @@ function exportSw(nextConfig) {
     const [defaultPathMap, { dev, distDir, outDir }] = args;
     const swDest = (nextConfig.workboxOpts && nextConfig.workboxOpts.swDest) || 'service-worker.js';
 
-    if (dev) {
-      return defaultPathMap;
+    if (!dev) {
+      // Copy service worker from Next.js build dir into the export dir.
+      await copy(join(distDir, swDest), join(outDir, swDest));
     }
-
-    // Copy service worker from Next.js build dir into the export dir.
-    await copy(join(distDir, swDest), join(outDir, swDest));
 
     // Run user's exportPathMap function if available.
     return nextConfig.exportPathMap ? nextConfig.exportPathMap(...args) : defaultPathMap;
