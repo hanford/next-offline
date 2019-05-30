f789e9c99cd8932c67a211c276b65d98065c1184
Raul <raul.abreu.leite@gmail.com>
2019-05-30 20:06:02 -0300

    Update networkFirst to v4
    Prevents the warning "deprecated networkFirst" from example.

--- examples/now2/next.config.js
+++ examples/now2/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
-        handler: 'networkFirst',
+        handler: 'NetworkFirst',
         options: {
           cacheName: 'https-calls',
           networkTimeoutSeconds: 15,
